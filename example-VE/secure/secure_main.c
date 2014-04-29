09c581e1e4b7cfb751a82f30dd6cb0dcba204d1a
shc5840 <shc5840@qq.com>
2014-04-29 15:38:12 +0800

    add NSACR CPACR SCR setting in secure status

--- example-VE/secure/secure_main.c
+++ example-VE/secure/secure_main.c
@@ -8,6 +8,12 @@ typedef unsigned char  u8;
 #define LED_ON	1
 #define LED_OFF	0
 
+#define Asm __asm__ volatile
+#define CP15_SET_NSACR(x)	Asm("mcr p15, 0, %0, c1, c1, 2"::"r"(x))
+#define CP15_SET_CPACR(x)	Asm("mcr p15, 0, %0, c1, c0, 2"::"r"(x))
+#define CP15_GET_SCR(x)		Asm("mrc p15, 0, %0, c1, c1, 0":"=r"(x))
+#define CP15_SET_SCR(x)		Asm("mrc p15, 0, %0, c1, c1, 0"::"r"(x))
+
 extern void monitorInit();
 
 void led_ctrl(u32 led_flag)
@@ -29,8 +35,24 @@ int secure_main(void)
 	unsigned int csu_reg, i;
 	for(csu_reg = 0x021C0000; csu_reg < 0x021C00A0; csu_reg = csu_reg + 4)
 		__REG(csu_reg) = 0x00ff00ff; //0x00330033 for Peripheral access policy
+		
+	// for CORAM secure policy setting
+	//csu_reg = 0x021C0000 + 0x**;
+	//__REG(csu_reg) = 0x00330033;
+	
+	// for OCRAM secure status setting
+	csu_reg = 0x020e0028;
+	__REG(csu_reg) = 0x00003810;
+	
+	// set for non-secure can access some coprocessor reg
+	CP15_SET_NSACR(0x00073fff);
+	CP15_SET_CPACR(0x0fffffff);
+	
+	// set for SCR
+	CP15_SET_SCR(0b110000);
 
 	//(*((void(*)(void))0x0090b020))();
+	// copy uboot from iram to sadram
 	char* dest = (char*)0x27800000;
 	char* src  = (char*)0x0090b000;
 	for(i=0; i<160096; i++)
