87bd588b50aebac8edf4050c4f69f35649119c0a
shc5840 <shc5840@qq.com>
2014-04-30 21:57:59 +0800

    fix the error mcr to mrc

--- example-VE/secure/secure_main.c
+++ example-VE/secure/secure_main.c
@@ -12,7 +12,7 @@ typedef unsigned char  u8;
 #define CP15_SET_NSACR(x)	Asm("mcr p15, 0, %0, c1, c1, 2"::"r"(x))
 #define CP15_SET_CPACR(x)	Asm("mcr p15, 0, %0, c1, c0, 2"::"r"(x))
 #define CP15_GET_SCR(x)		Asm("mrc p15, 0, %0, c1, c1, 0":"=r"(x))
-#define CP15_SET_SCR(x)		Asm("mrc p15, 0, %0, c1, c1, 0"::"r"(x))
+#define CP15_SET_SCR(x)		Asm("mcr p15, 0, %0, c1, c1, 0"::"r"(x))
 
 extern void monitorInit();
 
