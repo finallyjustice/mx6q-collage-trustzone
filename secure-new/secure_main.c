310212ac9206ca68626957219be0431cd4dff52f
shc5840 <shc5840@qq.com>
2014-05-19 11:04:51 +0800

    update secure-new, source architecture modify

--- secure-new/secure_main.c
+++ secure-new/secure_main.c
@@ -1,4 +1,63 @@
-#include "config.h"
+typedef unsigned long  u32;
+typedef unsigned short u16;
+typedef unsigned char  u8;
+#define __REG(x)        (*((volatile u32 *)(x)))
+#define __REG16(x)      (*((volatile u16 *)(x)))
+#define __REG8(x)       (*((volatile u8 *)(x)))
+
+#define Asm __asm__ volatile
+#define CP15_SET_VBAR(x)	Asm("mcr p15, 0, %0, c12, c0, 0"::"r"(x))
+#define CP15_SET_MVBAR(x)	Asm("mcr p15, 0, %0, c12, c0, 1"::"r"(x))
+#define CP15_SET_SCR(x)		Asm("mcr p15, 0, %0, c1,  c1, 0"::"r"(x))
+#define CP15_SET_NSACR(x)	Asm("mcr p15, 0, %0, c1,  c1, 2"::"r"(x))
+#define CP15_SET_CPACR(x)	Asm("mcr p15, 0, %0, c1,  c0, 2"::"r"(x))
+
+#define CP15_GET_SCR(x)		Asm("mrc p15, 0, %0, c1,  c1, 0":"=r"(x))
+
+//For Secure Configuration Register
+//=================================
+#define SCR_NET	(1 << 6)	/*Not Early Termination*/
+#define SCR_AW	(1 << 5)	/*0  the CPSR.A bit can be modified only in Secure state.*/
+							/*1  the CPSR.A bit can be modified in any security state.*/
+#define SCR_FW	(1 << 4)	/*0  the CPSR.F bit can be modified only in Secure state*/
+							/*1  the CPSR.F bit can be modified in any security state.*/
+#define SCR_EA	(1 << 3)	/*0  Abort mode handles external aborts*/
+							/*1  Monitor mode handles external aborts.*/
+#define SCR_FIQ	(1 << 2)	/*0  FIQ mode entered when FIQ is taken*/
+							/*1  Monitor mode entered when FIQ is taken.*/
+#define SCR_IRQ	(1 << 1)	/*0  IRQ mode entered when IRQ is taken*/
+							/*1  Monitor mode entered when IRQ is taken.*/
+#define SCR_NS	(1 << 0)	/*0:Secure state. 1:Non-secure state*/
+
+#define SCR_SETTING	( SCR_AW | SCR_FW )
+//=================================SCR
+
+//For Non-Secure Access Control Register
+//=================================
+#define NSACR_NS_SMP_BIT	(1 << 18)
+/*Determines if the SMP bit of the Auxiliary Control Register is writeable in Non-secure state*/
+
+#define NSACR_TL_BIT		(1 << 17)
+/*Determines if lockable TLB entries can be allocated in Non-secure state*/
+
+#define NSACR_PLE_BIT		(1 << 16)
+/*Controls NS accesses to the Preload Engine resources*/
+
+#define NSACR_CP11_BIT		(1 << 11)
+/*Determines permission to access coprocessor 11 in the Non-secure state*/
+
+#define NSACR_CP10_BIT		(1 << 10)
+/*Determines permission to access coprocessor 10 in the Non-secure state*/
+
+#define NSACR_SETTING (NSACR_NS_SMP_BIT | NSACR_TL_BIT |NSACR_PLE_BIT| NSACR_CP11_BIT | NSACR_CP10_BIT)
+//=================================end NSACR
+
+#define ID_RUN_UBOOT		(0xF0)
+#define ID_SWITCH_TO_NS		(0xF4)
+#define ID_SWITCH_TO_S		(0xF8)
+#define ID_EXE_CMD			(0xFC)
+#define ID_RUN_PRO			(0xE0)
+#define ID_EXE_CP15_WR		(0xE4)
 
 extern u32 monitor_vetor_table;
 
@@ -8,17 +67,17 @@ void delay(void)
 	for( i=0; i<0x100000; i++ );
 }
 
+#define LED_ON	1
+#define LED_OFF	0
 void led_ctrl(u32 led_flag)
 {
-    u32 led_reg;
 	delay();
 
-    led_reg = 0x020b4004;
     if( LED_ON == led_flag ){
-        __REG(led_reg) = 0x0;
+        __REG(0x020b4004) = 0x0;
     }
     else {
-        __REG(led_reg) = 0x3;
+        __REG(0x020b4004) = 0x3;
     }
 }
 
@@ -40,8 +99,7 @@ void target_csu_init(void)
 	
 	// for OCRAM secure status setting
 	// start address it 0, it means all internal ram will be use as secure ram	
-	reg = 0x020e0028;
-	__REG(reg) = 0x00003810; 
+	__REG(0x020e0028) = 0x00003810; 
 }
 
 // Snoop Control Unit
@@ -79,13 +137,13 @@ void target_gic_init(void)
 	for(reg = ICDISRnS; reg < ICDISRnE; reg = reg + 4)
 		__REG(reg) = 0xffffffff; 
 
-#if 1		
+#if 1
 	// ICDDCR Banked Distributor Control Register
 	reg = ICDDCR;
 	__REG(reg) = 0x3;
 	
 	reg = ICDSGIR;
-	__REG(reg) = 1<<15;
+	__REG(reg) = (1<<15);
 #endif	
 	// ICCBPR Banked Binary Point Register, this bit must setting in secure status in my opinion
 	reg = ICCBPR;
@@ -99,52 +157,32 @@ void target_gic_init(void)
 
 void target_init(void)
 {
-	u32 reg, i;	
+	u32 i;
 	char* dest = (char*)0x27800000;
 	char* src  = (char*)0x0090b000;	
 	
+	CP15_SET_MVBAR(&monitor_vetor_table);
+	
+	CP15_SET_SCR(0x30);
+	
+	CP15_SET_NSACR(0x00073fff);
+	
+	CP15_SET_CPACR(0x0fffffff); // Full access for cp11 and cp10
+
 	target_csu_init();
 	
 	target_scu_init();
-	
+
 	target_gic_init();
-	
-	// L2_Cache setting
-	reg = 0x00a02000 + 0x108;
-	__REG(reg) = 0x132;
-	
-	reg = 0x00a02000 + 0x10c;
-	__REG(reg) = 0x132;
-	
-	reg = 0x00a02000 + 0xf60;
-	__REG(reg) = 0x40800000;
-	
-	reg = 0x00a02000 + 0xf80;
-	__REG(reg) = 0x3;
-	
-	
+
 	// copy uboot from iram to sadram
 	for(i=0; i<160096; i++)
 		*dest++ = *src++;
-		
+
 #if 0	
 	//start uboot directly
 	(*((void(*)(void))0x27800000))(); 
 #endif	
-}
-
-void monitor_init(void)
-{
-	CP15_SET_MVBAR(&monitor_vetor_table);
-	
-	CP15_SET_SCR(SCR_SETTING);
-	
-	CP15_SET_NSACR(NSACR_SETTING);
-	
-	CP15_SET_CPACR(0x00f00000); // Full access for cp11 and cp10
-	
-	// for initialize target
-	target_init();
 }
 
 int secure_main(void)
